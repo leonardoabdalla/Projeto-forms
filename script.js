888cb3d5893722c047074ae426c1a13dbf79bef4
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2021-12-02 17:23:55 -0300

    requisito 3

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const buttonLogin = document.querySelector('#loginButton');
-const loginEmail = document.querySelector('#email').value;
-const loginPassword = document.querySelector('#password').value;
 
 function login() {
-  if (loginEmail.innerText === 'tryber@teste.com' && loginPassword.innerText === '123456') {
+  const loginEmail = document.querySelector('#email').value;
+  const loginPassword = document.querySelector('#password').value;
+  if (loginEmail === 'tryber@teste.com' && loginPassword === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos');
   }
+  
 }
 buttonLogin.addEventListener('click', login);
