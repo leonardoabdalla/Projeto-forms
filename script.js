b70bd096f795c32adc72ce231a97e5a1ac1af12e
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2021-12-03 09:47:01 -0300

    lint no javascript

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-//Chamando botão login
+// Chamando botão login
 const buttonLogin = document.querySelector('#loginButton');
 
-//Chamando botão enviar
+// Chamando botão enviar
 let buttonEnviar = document.querySelector('#submit-btn');
 
-//Chamando checkBox
+// Chamando checkBox
 const checkBox = document.querySelector('#agreement')
 
-//Iniciando button desativado
-//Referência:https://developer.mozilla.org/pt-BR/docs/Web/API/Element/setAttribute
+// Iniciando button desativado
+// Referência:https://developer.mozilla.org/pt-BR/docs/Web/API/Element/setAttribute
 buttonEnviar.setAttribute("disabled", true);
 
-//Função de habilitação do checkbox
+// Função de habilitação do checkbox
 function enviar() {
   console.log(checkBox.checked);
   if(checkBox.checked == true){
@@ -20,7 +20,7 @@ function enviar() {
 }
 checkBox.addEventListener("click", enviar);
 
-//Função de validação de login e senha-Requisito 3
+// Função de validação de login e senha-Requisito 3
 function login() {
   const loginEmail = document.querySelector('#email').value;
   const loginPassword = document.querySelector('#password').value;
